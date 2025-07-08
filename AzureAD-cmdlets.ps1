1eac3cdb2fe4afcc1131239304f4ed8b27d6cf13
JP <jerryperez06@gmail.com>
2025-07-08 15:43:32 -0400

    Add files via upload
    Manage Azure Active Directory (Azure AD) resources.

---
+++ AzureAD-cmdlets.ps1
@@ -0,0 +1,14 @@
+# Retrieves users from Azure Active Directory.
+Get-AzureADUser
+
+# Filter by UPN.
+Get-AzureADUser -ObjectId "user@domainhere.com"
+
+# Search by display name.
+Get-AzureADUser -SearchString "Bill"
+
+# Adds a user (or other object) to an Azure AD group.
+Add-AzureADGroupMember -ObjectId <GroupObjectId> -RefObjectId <UserObjectId>
+$group = Get-AzureADGroup -SearchString "IT"
+$user = Get-AzureADUser -SearchString "Bill Gates"
+Add-AzureADGroupMember -ObjectId $group.ObjectId -RefObjectId $user.ObjectId
