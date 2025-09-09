d7ce7bbbf8e314767d61cea25057282290129cc8
JP <jerryperez06@gmail.com>
2025-09-09 11:22:34 -0400

    Add files via upload
    PS script to an AD Security Groups passwords to not expire.
    Replace 'YourGroupName' with the name of your security group.

---
+++ ADUser-Password-Never-Expires.ps1
@@ -0,0 +1,12 @@
+﻿# PS script to an AD Security Groups passwords to not expire.
+# Replace 'YourGroupName' with the name of your security group.
+$groupName = "YourGroupName"
+
+# Get all members of the group
+$groupMembers = Get-ADGroupMember -Identity $groupName -Recursive | Where-Object { $_.objectClass -eq 'user' }
+
+# Set 'PasswordNeverExpires' to true for each user
+foreach ($user in $groupMembers) {
+    Set-ADUser -Identity $user.SamAccountName -PasswordNeverExpires $true
+    Write-Host "Set PasswordNeverExpires for user: $($user.SamAccountName)"
+}
